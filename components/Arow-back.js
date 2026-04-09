513f1c4a504bcf064e0048db7e5d22cd3eed7006
Chniak974 <urbaniak.nicolas62@gmail.com>
2026-04-09 13:27:10 +0400

    arrow position mail lucide

--- components/Arow-back.js
+++ components/Arow-back.js
@@ -1,17 +1,26 @@
-import React from "react";
-import { ChevronsLeft, MapPinned,Mail } from "lucide-react"
+"use client";
+import { ChevronsLeft, MapPinned, Mail } from "lucide-react"
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function ArowBack() {
+  const router = useRouter();
+
+  const forward = () => {
+   router.back();
+  };
   return (
     <>
-      <div className="w-10 h-10 flex justify-center items-center text-xl bg-b text-v transition duration-300 hover:bg-v hover:text-b rounded-full z-50 absolute left-3 bottom-5 sm:bottom-10 lg:bottom-15">
+      <div className="w-10 h-10 flex justify-center items-center text-xl bg-b text-v transition duration-300 hover:bg-v hover:text-b rounded-full z-50 absolute left-3 bottom-5 sm:bottom-10 lg:bottom-15"
+        onClick={() => forward()}>
         <ChevronsLeft />
       </div>
-      <div className="w-10 h-10 flex justify-center items-center text-xl bg-b text-v transition duration-300 hover:bg-v hover:text-b rounded-full z-50 absolute left-3 bottom-17 sm:bottom-22 lg:bottom-27">
+      <div className="w-10 h-10 flex justify-center items-center text-xl bg-b text-v transition duration-300 hover:bg-v hover:text-b rounded-full z-50 absolute left-3 bottom-17 sm:bottom-22 lg:bottom-27"
+      >
         <MapPinned />
       </div>
       <div className="w-10 h-10 flex justify-center items-center text-xl bg-b text-v transition duration-300 hover:bg-v hover:text-b rounded-full z-50 absolute left-3 bottom-29 sm:bottom-34 lg:bottom-39">
-        <Mail />
+        <Link href="/mail"><Mail /></Link>
       </div>
     </>
   );
