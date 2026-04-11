9e90edd8a2d489090f571b74b6f8da3ba0eda9f0
Chniak974 <urbaniak.nicolas62@gmail.com>
2026-04-11 13:13:34 +0400

    Cards

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="flex-1">
         <Banner />
         <Iris />
-        <main className="max-w-5xl mx-auto p-6 md:p-6">
+        <main className="max-w-7xl mx-auto p-6 md:p-6">
           <p className="text-center text-v text-sm pb-6 md:pb-6"><strong>Ouverture mairie :</strong> du lundi au jeudi de 7h45 à 16h15 - <strong>Fermeture</strong> à 15h le vendredi <br/> Contact: 0 262 39 50 50 </p>
           <h1 className="text-xl text-center  font-bold text-b mb-8 md:text-4xl">Bienvenue à l'Entre-Deux</h1>
             <Cards />
