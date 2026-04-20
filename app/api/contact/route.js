3ca6b1cd07e2327859344757669f4db746324ea4
Chniak974 <urbaniak.nicolas62@gmail.com>
2026-04-20 11:05:42 +0400

    footer

---
+++ app/api/contact/route.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export async function POST(){
+
+ return (
+    <></>
+  )
+}
--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='w-full relative p-4 bg-b flex flex-3'>
+      <div className='relative flex flex-1/3'>
+        <Image src="/footer-logo.png"
+               alt="Commune de L'Entre-Deux"
+               width={20}
+               height={20}
+               className=' border-2 border-[#8bfdfd] hover:shadow-emerald-300/50'
+               loading='eager' 
+               priority={true} />
+      </div>
+    </div>
   )
 }
